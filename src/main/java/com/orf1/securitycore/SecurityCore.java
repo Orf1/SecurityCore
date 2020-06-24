03e97f10df1960d1875cbb5901cb2c17a85d31ff
Orfeas <orfeas126@gmail.com>
2020-06-24 01:35:11 -0700

    Updated Code

--- src/main/java/com/orf1/securitycore/SecurityCore.java
+++ src/main/java/com/orf1/securitycore/SecurityCore.java
@@ -1,8 +1,10 @@
 package com.orf1.securitycore;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -35,6 +37,11 @@ boolean authenticated = false;
     @EventHandler
     public void onLogin(PlayerLoginEvent e){
         System.out.println("[SecurityCore] Player logged in with IP:" + e.getRealAddress());
+
+    }
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e){
+        e.getPlayer().sendMessage(ChatColor.GREEN + "[SecurityCore] " + ChatColor.WHITE + "Welcome!");
     }
 
     public void registerCommands(){
