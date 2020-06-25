1e46da6a06a2bc9acae1fb246558bf6fbe4c10d6
Orfeas <orfeas126@gmail.com>
2020-06-25 00:42:34 -0700

    Update

--- src/main/java/com/orf1/securitycore/RegisterCommand.java
+++ src/main/java/com/orf1/securitycore/RegisterCommand.java
@@ -7,6 +7,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
+
 public class RegisterCommand implements CommandExecutor {
 
     private Main main = JavaPlugin.getPlugin(Main.class);
@@ -16,6 +18,11 @@ public class RegisterCommand implements CommandExecutor {
         if (sender instanceof Player){
             Player player = (Player) sender;
             main.getTestFile().createSection("Users."+ player.getUniqueId());
+            try {
+                main.getTestFile().save(main.getFile());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             player.sendMessage(ChatColor.GREEN + "[SecurityCore] " + ChatColor.WHITE + "You have been registered.");
         }else{
             System.out.println("This command can only be used in-game.");
