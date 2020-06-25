8003dc74b964073a3046adca4be4c81e43e33733
Orfeas <orfeas126@gmail.com>
2020-06-25 01:21:36 -0700

    Update

--- src/main/java/com/orf1/securitycore/RegisterCommand.java
+++ src/main/java/com/orf1/securitycore/RegisterCommand.java
@@ -17,24 +17,36 @@ public class RegisterCommand implements CommandExecutor {
 
     private Main main = JavaPlugin.getPlugin(Main.class);
     private HashMap<Player, String> userPins = new HashMap<>();
-
+    private String pin;
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player){
             Player player = (Player) sender;
-            main.getTestFile().set("Users", player);
-            userPins.put(player, "1234");
-            main.getTestFile().createSection("Users");
-            main.getTestFile().set("Users", userPins);
-            try {
-                main.getTestFile().save(main.getFile());
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (args[0].isEmpty()) {
+                player.sendMessage("Empty");
             }
+            else if (args[0].length() > 16 || args[0].length() < 4){
+                player.sendMessage("Too Short/long");
+
+            }else {
+                pin = args[0];
+            }
+            userPins.put(player, pin);
+            writeFile(userPins);
+
             player.sendMessage(ChatColor.GREEN + "[SecurityCore] " + ChatColor.WHITE + "You have been registered.");
         }else{
             System.out.println("This command can only be used in-game.");
         }
         return false;
+    }
+    public void writeFile(HashMap data){
+        main.getTestFile().createSection("Users");
+        main.getTestFile().set("Users", data);
+        try {
+            main.getTestFile().save(main.getFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
