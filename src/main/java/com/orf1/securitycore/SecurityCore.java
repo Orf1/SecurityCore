e295fd0478b578d0291e38fef20f4f8b8cdd45fa
Orfeas <orfeas126@gmail.com>
2020-06-24 17:00:01 -0700

    Updated

--- src/main/java/com/orf1/securitycore/SecurityCore.java
+++ src/main/java/com/orf1/securitycore/SecurityCore.java
@@ -2,48 +2,29 @@ package com.orf1.securitycore;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-import java.io.IOException;
 
 public final class SecurityCore extends JavaPlugin implements Listener {
 
-boolean authenticated = false;
-private File playerData;
-private YamlConfiguration modifyPlayerData;
+boolean debug = false;
+
     @Override
     public void onEnable() {
-        System.out.println("[SecurityCore] Plugin initializing");
+        System.out.println("[SecurityCore] Initializing");
         System.out.println("[SecurityCore] Server Version: " + Bukkit.getVersion());
+
+        loadConfig();
+        checkAuthentication();
         registerEvents();
         registerCommands();
-        loadConfig();
-        try {
-            initiateFiles();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("[SecurityCore] Plugin initialization complete");
-        System.out.println("[SecurityCore] Starting authentication");
-        if (authenticate()){
-            System.out.println("[SecurityCore] Authentication complete");
-            authenticated = true;
-        }else{
-            System.out.println("[SecurityCore] Authentication failed");
-            authenticated = false;
-            Bukkit.getPluginManager().disablePlugin(this);
-        }
 
+        System.out.println("[SecurityCore] Initialization complete");
     }
-
-
 
     @Override
     public void onDisable() {
@@ -51,45 +32,40 @@ private YamlConfiguration modifyPlayerData;
     }
 
     @EventHandler
-    public void onLogin(PlayerLoginEvent e){
+    public void onLogin(PlayerLoginEvent e) {
         System.out.println("[SecurityCore] Player logged in with IP:" + e.getRealAddress());
-
     }
+
     @EventHandler
-    public void onJoin(PlayerJoinEvent e){
+    public void onJoin(PlayerJoinEvent e) {
         e.getPlayer().sendMessage(ChatColor.GREEN + "[SecurityCore] " + ChatColor.WHITE + "Welcome!");
     }
 
-    public void registerCommands(){
+    public void registerCommands() {
         getCommand("securitycore").setExecutor(new SecurityCoreCommand());
         getCommand("login").setExecutor(new LoginCommand());
         getCommand("register").setExecutor(new RegisterCommand());
-        getCommand("authenticate").setExecutor(new AuthenticateCommand());
-        getCommand("moose").setExecutor(new MooseCommand());
+        getCommand("setup").setExecutor(new SetupCommand());
     }
-    public void registerEvents(){
+
+    public void registerEvents() {
         Bukkit.getPluginManager().registerEvents(this,this);
     }
-    public File getFile(){
-        return playerData;
-    }
-    public boolean authenticate(){
 
-        return this.getConfig().getBoolean("auth");
+    private void checkAuthentication() {
+        System.out.println("[SecurityCore] Starting authentication");
+
+        if (this.getConfig().getBoolean("auth")) {
+            System.out.println("[SecurityCore] Authentication complete");
+        }
+        else {
+                System.out.println("[SecurityCore] Authentication failed");
+                Bukkit.getPluginManager().disablePlugin(this);
+        }
     }
+
     public void loadConfig() {
         this.getConfig().options().copyDefaults();
         saveDefaultConfig();
     }
-    public void initiateFiles() throws IOException {
-        playerData = new File(this.getDataFolder(), "playerdata.yml");
-        if (!playerData.exists()){
-            playerData.createNewFile();
-        }
-        modifyPlayerData = YamlConfiguration.loadConfiguration(playerData);
-    }
-    public YamlConfiguration getPlayerData(){
-        return modifyPlayerData;
-    }
-
 }
--- src/main/java/com/orf1/securitycore/SetupCommand.java
+++ src/main/java/com/orf1/securitycore/SetupCommand.java
@@ -4,7 +4,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class RegisterCommand implements CommandExecutor {
+public class SetupCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         return false;
