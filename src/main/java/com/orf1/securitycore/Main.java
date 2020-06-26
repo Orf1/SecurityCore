33e2574a134554d84288311fd0558406c83a3140
Orfeas <orfeas126@gmail.com>
2020-06-26 16:33:37 -0700

    Optimizations
    Optimized imports in Main.java

--- src/main/java/com/orf1/securitycore/Main.java
+++ src/main/java/com/orf1/securitycore/Main.java
@@ -8,7 +8,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
